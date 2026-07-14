1d1dd714fc39ad4ae3fcb7383663a997ee54f46f
uzzzzy <a.fauzy4b@gmail.com>
2026-07-14 11:36:48 +0800

    feat: implement in-memory TaskRepository for managing task CRUD operations

---
+++ repository.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"errors"
+)
+
+type TaskRepository struct {
+	tasks  []Task
+	nextID int
+}
+
+func NewTaskRepository() *TaskRepository {
+	return &TaskRepository{
+		tasks:  []Task{},
+		nextID: 1,
+	}
+}
+
+func (r *TaskRepository) Create(payload CreateTaskRequest) Task {
+	newTask := Task{
+		Id:        r.nextID,
+		Title:     payload.Title,
+		Completed: false,
+	}
+
+	r.tasks = append(r.tasks, newTask)
+	r.nextID++
+	return newTask
+}
+
+func (r *TaskRepository) GetAll() []Task {
+	return r.tasks
+}
+
+func (r *TaskRepository) GetById(id int) (Task, error) {
+	for _, task := range r.tasks {
+		if task.Id == id {
+			return task, nil
+		}
+	}
+
+	return Task{}, errors.New("Task Not Found")
+}
+
+func (r *TaskRepository) UpdateById(id int, payload UpdateTaskRequest) (Task, error) {
+	for i, task := range r.tasks {
+		if task.Id == id {
+			r.tasks[i].Title = payload.Title
+			r.tasks[i].Completed = payload.Completed
+
+			return r.tasks[i], nil
+		}
+	}
+
+	return Task{}, errors.New("Task Not Found")
+}
+
+func (r *TaskRepository) DeleteById(id int) (Task, error) {
+	for i, task := range r.tasks {
+		if task.Id == id {
+			r.tasks = append(r.tasks[:i], r.tasks[i+1:]...)
+			return r.tasks[i], nil
+		}
+	}
+
+	return Task{}, errors.New("Task Not Found")
+}
